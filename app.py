b75f1129af2995db7b9ae0510062457ef60e6fc7
anushkah-01 <anushkahirloskar@gmail.com>
2025-11-05 14:32:21 +0530

    Fix: updated local market page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, auth
 
 cred = credentials.Certificate({
- "Google_cloud_credentials"
+"Google Cloud Credentials"
 })
 
 firebase_admin.initialize_app(cred)
